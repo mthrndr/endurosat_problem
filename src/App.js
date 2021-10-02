cefdf66b5bb8733b2a9961639690fd295bed8358
mthrndr <kristian.d.nikolov@gmail.com>
2021-10-02 20:36:13 +0200

    Added PowerContextProvider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import ParameterDisplay from './components/ParameterDisplay';
 import NotificationsPanel from './components/NotificationsPanel';
 import CommandPanel from './components/CommandPanel';
 
+import PowerContextProvider from './PowerContextProvider';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         test
+      </header>
+      <PowerContextProvider>
         <ParameterDisplay />
         <ParameterDisplay />
         <ParameterDisplay />
         <ParameterDisplay />
         <CommandPanel />
         <NotificationsPanel />
-      </header>
+      </PowerContextProvider>
     </div>
   );
 }
